864f0483e8f769481931de419945a1e4f1d39172
jacobshultz <jacob.shultz.05@gmail.com>
2026-04-13 08:46:03 -0400

    halfway through projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,25 @@
 import Return from "@/components/return";
+import { getAllProjects } from "@/lib/project";
 
 export default function Projects() {
+    const projects = getAllProjects();
+
     return (
         <>
             <Return/>
-            <h1 style={{textAlign: "center"}}>My Work</h1>
+            <h1 style={{textAlign: "center"}}>My Work Experience</h1>
+            <p>For a complete list of projects I've completed, please visit my <a className="bigger" href="https://github.com/jacobshultz">github</a></p>
+            <p>These are just the most impressive ones (for now at least).</p>
+            <ul>
+                {projects.map((project) => (
+                    <li className="project-item">
+                        <div className="topper">
+                            <h2>{project.title}</h2>
+                            <p>{project.internship}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </>
     );
 }
